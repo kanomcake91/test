eca3d9393c3ed6be4683d5dd36cd8c3ef1d02519
Sorravit <kanomcake1928@gmail.com>
2020-04-22 01:23:17 +0700

    Show score confirmed deaths and recovered

--- js/country.js
+++ js/country.js
@@ -7,7 +7,6 @@ $(function () {
   var url = "https://pomber.github.io/covid19/timeseries.json";
   $.getJSON(url, function (result) {
     var selectedCountry = result[country];
-    console.log(selectedCountry);
     for (var i = 0; i < selectedCountry.length; i++) {
       var row = `<tr>
         <th scope="row">${selectedCountry[i].date}</th>
@@ -17,15 +16,5 @@ $(function () {
                 </tr>`;
       $("#data").append(row);
     }
-  });
-  $.getJSON(url, function (result) {
-    var row2 = `
-    <?php
-      $csv = fopen("https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv");
-      while(!feof($csv)){
-        print_r(fgetcsv($csv[0]))
-      }
-    ?>`;
-    $("#gg").append(row2);
   });
 });
--- js/data.js
+++ js/data.js
@@ -3,6 +3,9 @@ $(function () {
   $.getJSON(url, function (result) {
     console.log(result);
     var no = 1;
+    var confirmed = 0;
+    var deaths = 0;
+    var recovered = 0;
     for (var country in result) {
       var row = `<tr>
                           <th scope="row">${no}</th>
@@ -11,5 +14,15 @@ $(function () {
       $("#data").append(row);
       no++;
     }
+    for (var country in result) {
+      var selectedCountry = result[country];
+      var total = selectedCountry.length;
+      confirmed = confirmed + selectedCountry[total - 1].confirmed;
+      deaths = deaths + selectedCountry[total - 1].deaths;
+      recovered = recovered + selectedCountry[total - 1].recovered;
+    }
+    $("#confirmed").append(confirmed);
+    $("#deaths").append(deaths);
+    $("#recovered").append(recovered);
   });
 });
