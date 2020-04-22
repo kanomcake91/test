3f7273bb58c12e849ced0e59090fa83ce5bbb81d
Sorravit <kanomcake1928@gmail.com>
2020-04-22 19:42:06 +0700

    Show score confirmed deaths and recovered v.2

--- js/country.js
+++ js/country.js
@@ -4,17 +4,29 @@ $(function () {
   var country = urlParams.get("country");
   $("#country").html(country);
   var Total = 0;
+  var confirmed = 0;
+  var deaths = 0;
+  var recovered = 0;
   var url = "https://pomber.github.io/covid19/timeseries.json";
   $.getJSON(url, function (result) {
     var selectedCountry = result[country];
-    for (var i = 0; i < selectedCountry.length; i++) {
+    var total = selectedCountry.length;
+    for (var i = 0; i < total; i++) {
       var row = `<tr>
         <th scope="row">${selectedCountry[i].date}</th>
         <td>${selectedCountry[i].confirmed}</td>
         <td>${selectedCountry[i].deaths}</td>
         <td>${selectedCountry[i].recovered}</td>
                 </tr>`;
+
       $("#data").append(row);
     }
+    var confirmed = selectedCountry[total - 1].confirmed;
+    var deaths = selectedCountry[total - 1].deaths;
+    var recovered = selectedCountry[total - 1].recovered;
+
+    $("#confirmed").append(confirmed);
+    $("#deaths").append(deaths);
+    $("#recovered").append(recovered);
   });
 });
