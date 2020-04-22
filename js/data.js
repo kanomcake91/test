e739f8850348d19f3b2327cd8981c0c5413a3d18
Sorravit <kanomcake1928@gmail.com>
2020-04-22 18:45:16 +0700

    graph total score comfirmed deaths and recovered

--- js/data.js
+++ js/data.js
@@ -24,5 +24,40 @@ $(function () {
     $("#confirmed").append(confirmed);
     $("#deaths").append(deaths);
     $("#recovered").append(recovered);
+    var ctxB = document.getElementById("barChart").getContext("2d");
+    var myBarChart = new Chart(ctxB, {
+      type: "bar",
+      data: {
+        labels: ["Confirmed", "Deaths", "Recovered"],
+        datasets: [
+          {
+            label: "Total",
+            data: [confirmed, deaths, recovered],
+            backgroundColor: [
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+            ],
+            borderColor: [
+              "rgba(54, 162, 235, 1)",
+              "rgba(255,99,132,1)",
+              "rgba(255, 206, 86, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
   });
 });
